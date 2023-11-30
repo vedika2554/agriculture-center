c0c44cad5848add5b787b8b26d9686da00a451bd
achalkatkar <11achalkatkar@gmail.com>
2023-11-30 17:44:34 +0530

    added client and server setup

---
+++ server/index.js
@@ -0,0 +1,22 @@
+import express from 'express';
+import dotenv from 'dotenv';
+import mongoose from 'mongoose';
+dotenv.config();
+
+const app = express();
+app.use(express.json());
+
+const connectMongoDB = async () =>{
+  const connection = await mongoose.connect(process.env.MONGODB_URI);
+  if(connection){
+    console.log("Connected to MongoDB");
+  }
+}
+connectMongoDB();
+
+const PORT = 5000;
+
+
+app.listen(PORT, () => {
+  console.log(`Server listening on port ${PORT} 🚀`);
+});
