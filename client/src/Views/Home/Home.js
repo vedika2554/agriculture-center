4db6d39ce6847b4a702962ee2244128bd34a762a
atharva1jari <atharvawanjari09@gmail.com>
2023-12-03 16:31:09 +0530

    some file adds

---
+++ client/src/Views/Home/Home.js
@@ -0,0 +1,43 @@
+import React from 'react';
+import "./Home.css";
+
+<body>
+    <div class="Nav-Container">
+        <div>
+        <a class=".Nav-heading1">Agliculture Center</a>
+        </div>
+        <div>
+       <a class="Nav-heading">Home</a>
+       <a class="Nav-heading">Login</a>
+       <a class="Nav-heading">Sign-Up</a>
+       <a class="Nav-heading">About</a>
+    </div>
+    </div>
+    <h1>Agliculture Center</h1>
+
+    <div class="Container">
+     
+        <div class="card">
+            
+       </div>
+       <div class="card">
+            
+       </div>
+
+       <div class="card">
+            
+       </div>
+
+       <div class="card">
+            
+       </div>
+
+    </div>
+
+    <footer>
+        <p>@ Content owned by Directorate of Knowledge Mangement in Agricultural; Agriculture centre 2023</p>
+        <p>Copyrights & 2022 All Rights Reserved By Indian Councilof Agricultural</p>
+    </footer>
+
+
+</body>
