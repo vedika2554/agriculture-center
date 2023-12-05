ccf532c695a39b4f9f98344f709cd6933961a626
vedika2554 <vedikayadav652@gmail.com>
2023-12-05 11:04:50 +0530

    flower fruit is ready

--- client/src/index.js
+++ client/src/index.js
@@ -1,7 +1,28 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import {createBrowserRouter, RouterProvider} from 'react-router-dom'
+import Fruit from './views/Fruit/Fruit'
+import Flowers from './views/Flowers/Flowers'
+
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
+
+const router = createBrowserRouter([
+  {
+  'path': '/fruits',
+  'element': <Fruit/>
+},
+{
+  'path': '/flowers',
+  'element': <Flowers/>
+},
+
+
+])
+
+
+
 root.render(
-  <h1>Home</h1>
+  <RouterProvider router={router}/>
+ 
 )
--- server/index.js
+++ server/index.js
@@ -1,8 +1,9 @@
 import express from 'express';
 import dotenv from 'dotenv';
-import mongoose from 'mongoose';
 dotenv.config();
-
+import mongoose from 'mongoose';
+import Flowers from './models/Flowers.js'
+import Fruits from './models/Fruits.js'
 const app = express();
 app.use(express.json());
 
@@ -13,6 +14,202 @@ const connectMongoDB = async () =>{
   }
 }
 connectMongoDB();
+
+app.post('/flower', async (req,res)=>{
+  const { name, price, description, image } = req.body;
+
+  const flower = new Flowers({
+    name: name,
+    price: price,
+    description: description,
+    image: image
+  });
+  try{
+
+    const savedFlowers = await flower.save();
+  
+      res.json({
+          success: true,
+          data: savedFlowers,
+          message: 'flowers added successfully'
+      })
+  }catch(e){
+      res.json({
+          success:false,
+          message: e.message
+      })
+  }
+})
+
+
+
+
+app.get('/flowers', async(req, res)=>{
+
+  const flowers = await Flowers.find();
+  
+     res.json({
+      success: true,
+      data: flowers,
+      message: 'flowes get successfully'
+     }) 
+  })
+  
+  app.get('/flowers/:id', async(req, res)=>{
+      const {id} = req.params;
+  
+      const flowers = await Flowers.findOne({_id: id});
+      res.json({
+          success: true,
+          data: flowers,
+          message: 'flowers retrive successfully'
+      })
+  })
+
+
+
+
+  app.delete('/flowers/:id', async (req, res)=>{
+    const {id} = req.params;
+
+    const flower = await Flowers.deleteOne({_id: id});
+    res.json({
+        success: true,
+        data: flower,
+        message: 'flower deleted successfully'
+    })
+
+})
+app.put('/flower/:id', async (req, res)=>{
+    const {id} = req.params;
+    const { name, price, description, image } = req.body;
+
+    await Flowers.updateOne({_id: id}, {$set:{
+        name: name,
+        price: price,
+        description: description,
+        image: image
+ }});
+ const updatedFlower = await Flowers.findOne({_id: id});
+
+ res.json({
+    success: true,
+    data: updatedFlower,
+    message: 'Flower update successfully'
+ })
+})
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+app.post('/fruit', async (req,res)=>{
+  const { name, price, description, image } = req.body;
+
+  const fruit = new Fruits({
+    name: name,
+    price: price,
+    description: description,
+    image: image
+  });
+  try{
+
+    const savedFruits = await fruit.save();
+  
+      res.json({
+          success: true,
+          data: savedFruits,
+          message: 'fruits added successfully'
+      })
+  }catch(e){
+      res.json({
+          success:false,
+          message: e.message
+      })
+  }
+})
+
+
+
+
+app.get('/fruits', async(req, res)=>{
+
+  const fruits = await Fruits.find();
+  
+     res.json({
+      success: true,
+      data: fruits,
+      message: 'fruits get successfully'
+     }) 
+  })
+  
+  app.get('/fruits/:id', async(req, res)=>{
+      const {id} = req.params;
+  
+      const fruits = await Fruits.findOne({_id: id});
+      res.json({
+          success: true,
+          data: fruits,
+          message: 'fruits retrive successfully'
+      })
+  })
+
+
+
+
+  app.delete('/fruits/:id', async (req, res)=>{
+    const {id} = req.params;
+
+    const fruit = await Fruits.deleteOne({_id: id});
+    res.json({
+        success: true,
+        data: fruit,
+        message: 'fruit deleted successfully'
+    })
+
+})
+app.put('/fruit/:id', async (req, res)=>{
+    const {id} = req.params;
+    const { name, price, description, image } = req.body;
+
+    await Fruits.updateOne({_id: id}, {$set:{
+        name: name,
+        price: price,
+        description: description,
+        image: image
+ }});
+ const updatedFruit = await Fruits.findOne({_id: id});
+
+ res.json({
+    success: true,
+    data: updatedFruit,
+    message: 'Fruit update successfully'
+ })
+})
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 const PORT = 5000;
 
