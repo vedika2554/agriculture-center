7e70b90e7f3b88130f5101a111fe15e2e2cadc31
vedika2554 <vedikayadav652@gmail.com>
2023-12-07 22:26:35 +0530

    git pull

---
+++ client/src/View/Buyfruit/Buyfruit.js
@@ -0,0 +1,79 @@
+import React, { useEffect, useState } from "react";
+import { useParams } from "react-router-dom";
+import axios from "axios";
+import "./Buyfruit.css";
+
+export default function Buy(){
+    const {id} = useParams();
+    const [user, setUser] = useState({});
+    const [fruit,setFruit] = useState({});
+    const [quantity,setQuantity] = useState(1);
+    const [shippingAddress,setShippingAddress] = useState('');
+
+    const loadFruit = async()=>{
+
+        if(!id){
+            window.location.href = "/";
+        }
+
+         const response = await axios.get(`/fruits/${id}`)
+     
+
+        setFruit(response.data.data);
+    };
+
+
+
+    const increase = async()=>{
+        setQuantity(quantity+1);
+    }
+
+    const decrease =async()=>{
+        if(quantity>1){
+            setQuantity(quantity-1);
+        }
+    }
+    useEffect(()=>{
+        loadFruit();
+    },[]);
+
+
+    const placeorder =async()=>{
+        const response = await axios.post("/order",{
+            fruit:fruit,
+            user:user._id,
+            quantity:quantity,
+            shippingAddress:shippingAddress,
+        })
+
+        alert(response.data.message);
+        window.location.href = "/orders"
+    }
+    return(
+        <div className="buy-container">
+            <img src={fruit.image} alt={fruit.name} className="buy-product-img"/>
+            <div>
+                <h1>{fruit.name}</h1>
+                <p>{fruit.description}</p>
+                <h1>{fruit.price}</h1>
+
+            <div className="qt-container">
+                <span className="quantity-btn" onClick={decrease}>-</span>
+                <span className="quantity-text">{quantity} </span>
+                <span className="quantity-btn" onClick={increase}>+</span>
+            </div>
+
+            <input type="text"
+                placeholder="shippingAddress"
+                className="shippingAddress"
+                value={shippingAddress}
+                onChange={(e)=>{
+                    setShippingAddress(e.target.value);
+                }}
+                />
+
+                <button className="Buy-btn" type="button" onClick={placeorder}>PlaceOrder</button>
+            </div>
+        </div>
+    )
+}
--- client/src/index.js
+++ client/src/index.js
@@ -4,6 +4,8 @@ import Flowers from  './View/Flowers/Flowers'
 import Nearbystore from './View/Nearbystore/Nearbystore'
 
 import Home from './View/Home/Home';
+import Buyflower from './View/Buyflower/Buyflower'
+import Buyfruit from './View/Buyfruit/Buyfruit'
 import { RouterProvider, createBrowserRouter } from 'react-router-dom';
 
 
@@ -25,8 +27,15 @@ const router = createBrowserRouter([
   {
         path: "/",
         element: <Home/>
-    }
-
+    },
+    {
+      path: "/buyflower/:id",
+      element: <Buyflower/>
+  },
+  {
+    path: "/buyfruit/:id",
+    element: <Buyfruit/>
+},
 ])
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
