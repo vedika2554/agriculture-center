1188b578dcea3b81eddb282b83599529b64c0826
vedika2554 <vedikayadav652@gmail.com>
2024-01-26 14:38:32 +0530

    jhfhf

---
+++ client/src/View/Flowers/Flowers.js
@@ -0,0 +1,44 @@
+import React, { useEffect, useState } from "react";
+import FlowerCard from '../../Component/Flowercard/Flowercard'
+import axios from "axios";
+import './Flowers.css';
+import Navbar from '../../Component/Navbar/Navbar';
+import Footer from "../../Component/Footers/footer";
+
+function Flower(){
+
+    const[flower, setFlower] = useState([]);
+
+
+    const loadFlower = async () => {
+        const responce = await axios.get('/flowers');
+        setFlower(responce?.data?.data)
+    }
+
+    useEffect(()=>{
+        loadFlower();
+    })
+    return(
+        <>
+        <Navbar/>
+        <div className="flower">
+            <h1 className="flower-tital">Summer flowers</h1>
+            <div className="flowers-container">
+                {
+                    flower?.map((flower, index)=>{
+                        const {_id, name, price, description, image} = flower;
+                        return(
+                            <FlowerCard key={index} id={_id} name={name} price={price}
+                            description={description} image={image}/> )
+                    })
+                }
+            </div>
+            <h1 className="flower-tital1">Mansoom flowers</h1>
+            <h1 className="flower-tital2">Winter flowers</h1>
+
+        </div>
+        <Footer/>
+        </>
+    )
+}
+export default Flower
